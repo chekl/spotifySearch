be9e26be4f50d802af376140630e73e3e265131a
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2023-02-26 01:26:54 +0200

    add styles

--- src/components/Card/CardGroup.jsx
+++ src/components/Card/CardGroup.jsx
@@ -2,30 +2,34 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea, Box } from '@mui/material';
-import "./CardGroup.css"
+import './CardGroup.css';
 
-export default function CardGroup({group}) {
+export default function CardGroup({ group }) {
   return (
-    <Card className="card">
-      <CardActionArea >
-      <Box className="cardContent">
-        <CardMedia
-          component="img"
-          image={group.images.length > 0 ? group.images[0].url : "paint.jpg"}
-          alt={group.name + " фото гурту"}
-          className='cardImg'
-        />
-                 <CardContent>
-          <h2>
-          {group.name}
-          </h2>
-          <p>
-            Кількість підписників: {group.followers.total} <br/>
-          </p>
-        </CardContent> 
-      </Box>
+    <Card className='card'>
+      <CardActionArea>
+        <Box className='cardContent'>
+          <CardMedia
+            component='img'
+            image={group.images.length > 0 ? group.images[0].url : 'noimage.png'}
+            alt={group.name + ' фото гурту'}
+            className='cardImg'
+          />
+          <CardContent>
+            <h2>{group.name}</h2>
+            <p>
+              Кількість підписників: {group.followers.total} <br />
+              </p>
+              <p>Жанр виконання: {group.genres.length !== 0 ? group.genres.map((genre, index) => {
+                if(index < 9) {
+                  return <span className='genreSpan'>{genre}</span>
+                }
+              }) : <span className='genreSpan'>відсутній</span>
+              }
+            </p>
+          </CardContent>
+        </Box>
       </CardActionArea>
     </Card>
   );

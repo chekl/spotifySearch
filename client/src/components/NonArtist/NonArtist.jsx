0389416ec60ae5cba21b85347ebe20a974cdfff8
Vladyslava Chaika <vladyslava.chaika@nure.ua>
2023-06-29 18:41:05 +0300

    add message "No artist found"

---
+++ client/src/components/NonArtist/NonArtist.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NonArtist.css';
+
+export default function NonArtist() {
+  return (
+    <div className='non'>
+      <img
+        src='/non-found.webp'
+        alt='Такого виконавця немає Hark!'
+        height='100px'
+      />
+      <p>На жаль, цього виконавця немає...</p>
+    </div>
+  );
+}
